082d72fb6fb984f5bd43e0d98fe3607e4dd48b88
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2022-01-26 11:39:12 -0500

    chore(files): cleanup

--- w2/solution.sql
+++ w2/solution.sql
@@ -1,32 +1,81 @@
 -- ***********************
--- Name: Fardeen Sohil Panjwani
--- ID: 119588218
--- Date: 19th Jan, 2022
+-- Name: John Doe
+-- ID: 55499271
+-- Date: 19th Jan, 3022
 -- Purpose: Lab 02 DBS211
 -- ***********************
 
 -- Q1 Solution --
-select officecode, 
+SELECT officecode, 
        city, 
        state, 
        country, 
        phone 
-from offices;
+FROM offices;
 
 -- Q2 Solution --
-select employeenumber,
+SELECT employeenumber,
        firstname,
        lastname,
        extension
-from employees
-where officecode = 1
-order by employeenumber;
+FROM employees
+WHERE officecode = 1
+ORDER BY employeenumber;
 
 -- Q3 Solution --
-select customernumber as "Customer Number",
-       customername as "Customer Name",
-       concat(concat(contactfirstname, ' '), contactlastname) as "Contact Name",
-       phone as "Phone"
-from customers
-where country like 'Canada'
-order by customername;
+SELECT customernumber AS "Customer Number",
+       customername AS "Customer Name",
+       CONCAT(CONCAT(contactfirstname, ' '), contactlastname) AS "Contact Name",
+       phone AS "Phone"
+FROM customers
+WHERE country LIKE 'Canada'
+ORDER BY customername;
+
+-- Q4 Solution --
+SELECT DISTINCT customernumber 
+FROM payments 
+ORDER BY customernumber 
+FETCH FIRST 10 ROWS ONLY;
+
+-- Q5 Solution --
+SELECT customernumber, 
+       checknumber, 
+       amount 
+FROM payments 
+WHERE amount NOT BETWEEN 1500 AND 120000 
+ORDER BY amount DESC;
+
+-- Q6 Solution --
+SELECT pa.productcode,
+       pa.productname,
+       pa.buyprice,
+       pa.msrp,
+       pa.markup,
+       round(pa.percmarkup, 1)
+FROM (
+    SELECT productcode,
+           productname,
+           buyprice,
+           msrp,
+           msrp - buyprice AS markup,
+		   100 * ((msrp - buyprice) / buyprice) AS percmarkup
+    FROM products
+) pa
+WHERE pa.percmarkup > 140
+ORDER BY pa.percmarkup;
+
+-- Q7 Solution --
+SELECT replace(productcode, '_', ' ') AS productcode,
+       productname, 
+       quantityinstock 
+FROM products 
+WHERE lower(productname) LIKE '%co%' 
+ORDER BY quantityinstock;
+
+-- Q8 Solution --
+SELECT customernumber,
+       contactfirstname,
+       contactlastname
+FROM customers
+WHERE lower(contactfirstname) LIKE 's%e%'
+ORDER BY customernumber;
