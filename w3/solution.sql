763e9ad9f6a78c9b1cf01b39291d614fa53a8170
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2022-01-28 15:38:19 -0500

    feat: question 5 solution

--- w3/solution.sql
+++ w3/solution.sql
@@ -42,3 +42,9 @@ DELETE FROM employees2 WHERE employeenumber = 1028414;
 -- Fails because there's another row in the table that has it's foreign key
 -- attached to the row being deleted
 -- This results in an "Integrity Constraint Violation"
+
+-- Q5 Solution --
+DELETE from employees2 WHERE employeenumber = 1028415;
+-- Running solution 4 after running this statement works because
+-- now there's no dependency being violated and the employee can be
+-- safely deleted from the table
