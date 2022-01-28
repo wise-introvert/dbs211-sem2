38b6996013798f4143c177faa762a0d14e52d0f3
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2022-01-28 15:32:12 -0500

    feat: question 2 solution

--- w3/solution.sql
+++ w3/solution.sql
@@ -10,3 +10,16 @@ SET AUTOCOMMIT ON;
 -- Q1 Solution --
 INSERT INTO employees2
 SELECT * FROM employees;
+
+-- Q2 Solution --
+INSERT INTO employees2
+VALUES(
+    1028414,
+    'Panjwani',
+    'Fardeen',
+    'x2222',
+    'fspanjwani@myseneca.ca',
+    '4',
+    1088,
+    'Head Cashier'
+);
