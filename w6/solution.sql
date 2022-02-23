8d68ab658d67180d965d4249837dabda880936ed
Fardeen Panjwani <fardeen.wi.panjwani@gmail.com>
2022-02-23 17:04:32 -0500

    refactor: update table name

--- w6/solution.sql
+++ w6/solution.sql
@@ -6,7 +6,7 @@
 -- ***********************
 
 -- Q1 SOLUTION --
-CREATE TABLE newEmployees 
+CREATE TABLE new_employees 
 AS
 (SELECT * FROM employees);
 
@@ -16,30 +16,30 @@ SET TRANSACTION READ WRITE;
 
 -- Q3 SOLUTION --
 INSERT ALL
-  INTO newEmployees VALUES (100, 'Patel', 'Ralph', 22333, 'rpatel@mail.com', 1, NULL, 'Sales Rep')
-  INTO newEmployees VALUES (101, 'Denis', 'Betty', 33444, 'bdenis@mail.com', 4, NULL, 'Sales Rep')
-  INTO newEmployees VALUES (102, 'Biri', 'Ben', 44555, 'bbirir@mail.com', 2, NULL, 'Sales Rep')
-  INTO newEmployees VALUES (103, 'Newman', 'Chad', 66777, 'cnewman@mail.com', 3, NULL, 'SalesRep')
-  INTO newEmployees VALUES (104, 'Ropeburn', 'Audrey', 77888, 'aropebur@mail.com', 1, NULL, 'Sales Rep')
+  INTO new_employees VALUES (100, 'Patel', 'Ralph', 22333, 'rpatel@mail.com', 1, NULL, 'Sales Rep')
+  INTO new_employees VALUES (101, 'Denis', 'Betty', 33444, 'bdenis@mail.com', 4, NULL, 'Sales Rep')
+  INTO new_employees VALUES (102, 'Biri', 'Ben', 44555, 'bbirir@mail.com', 2, NULL, 'Sales Rep')
+  INTO new_employees VALUES (103, 'Newman', 'Chad', 66777, 'cnewman@mail.com', 3, NULL, 'SalesRep')
+  INTO new_employees VALUES (104, 'Ropeburn', 'Audrey', 77888, 'aropebur@mail.com', 1, NULL, 'Sales Rep')
 SELECT * FROM dual;
 
 -- Q4 SOLUTION --
-SELECT * FROM newEmployees 
+SELECT * FROM new_employees 
 WHERE employeenumber BETWEEN 100 AND 104;
 -- number of rows selected:  5
 
 -- Q5 SOLUTION --
 ROLLBACK;
-SELECT * FROM newEmployees;
+SELECT * FROM new_employees;
 -- number of rows selected:  23
 
 -- Q6 SOLUTION --
 COMMIT;
-SELECT * FROM newEmployees;
+SELECT * FROM new_employees;
 -- number of rows selected:  23
 
 -- Q7 SOLUTION --
-UPDATE newEmployees 
+UPDATE new_employees 
 SET jobTitle = 'Unknown';
 
 -- Q8 SOLUTION --
@@ -48,7 +48,7 @@ COMMIT;
 -- Q9 SOLUTION --
 ROLLBACK;
 --     (a)     --
-SELECT * FROM newEmployees
+SELECT * FROM new_employees
 WHERE jobTitle = 'Unknown';
 -- number of rows selected:  23 ( all of them are still updated )
 
@@ -65,17 +65,17 @@ WHERE jobTitle = 'Unknown';
 -- made earlier permanent and irreversible
 
 -- Q10 SOLUTION --
-DELETE FROM newEmployees;
+DELETE FROM new_employees;
 
 -- Q11 SOLUTION --
 CREATE VIEW vwNewEmps 
 AS 
-SELECT * FROM newEmployees
+SELECT * FROM new_employees
 ORDER by lastname , firstname;
 
 -- Q12 SOLUTION --
 ROLLBACK;
-SELECT COUNT(*) FROM newEmployees;
+SELECT COUNT(*) FROM new_employees;
 
 --     (a)     --
 -- 0
@@ -86,55 +86,55 @@ SELECT COUNT(*) FROM newEmployees;
 
 -- Q13 SOLUTION --
 INSERT ALL
-  INTO newEmployees VALUES (100, 'Patel', 'Ralph', 22333, 'rpatel@mail.com', 1, NULL, 'Sales Rep')
-  INTO newEmployees VALUES (101, 'Denis', 'Betty', 33444, 'bdenis@mail.com', 4, NULL, 'Sales Rep')
-  INTO newEmployees VALUES (102, 'Biri', 'Ben', 44555, 'bbirir@mail.com', 2, NULL, 'Sales Rep')
-  INTO newEmployees VALUES (103, 'Newman', 'Chad', 66777, 'cnewman@mail.com', 3, NULL, 'SalesRep')
-  INTO newEmployees VALUES (104, 'Ropeburn', 'Audrey', 77888, 'aropebur@mail.com', 1, NULL, 'Sales Rep')
+  INTO new_employees VALUES (100, 'Patel', 'Ralph', 22333, 'rpatel@mail.com', 1, NULL, 'Sales Rep')
+  INTO new_employees VALUES (101, 'Denis', 'Betty', 33444, 'bdenis@mail.com', 4, NULL, 'Sales Rep')
+  INTO new_employees VALUES (102, 'Biri', 'Ben', 44555, 'bbirir@mail.com', 2, NULL, 'Sales Rep')
+  INTO new_employees VALUES (103, 'Newman', 'Chad', 66777, 'cnewman@mail.com', 3, NULL, 'SalesRep')
+  INTO new_employees VALUES (104, 'Ropeburn', 'Audrey', 77888, 'aropebur@mail.com', 1, NULL, 'Sales Rep')
 SELECT * FROM dual;
 
 -- Q14 SOLUTION --
 SAVEPOINT insertion;
 
 -- Q15 SOLUTION --
-UPDATE newEmployees 
+UPDATE new_employees 
 SET jobTitle = 'Unknown';
-SELECT * FROM newEmployees
+SELECT * FROM new_employees
 WHERE jobTitle = 'Unknown';
 
 -- Q16 SOLUTION --
 ROLLBACK to SAVEPOINT insertion;
-SELECT * FROM newEmployees;
+SELECT * FROM new_employees;
 -- The data fetched is similar to what was saved at the SAVEPOINT. 
 -- The rollback command, coupled with the SAVEPOINT flag, reverted all changes
 -- made upto the savepoint.
 
 -- Q17 SOLUTION --
 ROLLBACK;
-SELECT * FROM newemployees;
+SELECT * FROM new_employees;
 -- The rollback command cleared any uncommited changes made after the save point
 -- and the fetched data reflects this.
 
 -- Q18 SOLUTION --
 REVOKE all
-ON newEmployees
+ON new_employees
 FROM PUBLIC;
 
 -- Q19 SOLUTION --
 GRANT READ
-ON newEmployees
+ON new_employees
 TO dbs211_221zb16;
 
 -- Q20 SOLUTION --
 GRANT INSERT, UPDATE, DELETE
-ON newEmployees
+ON new_employees
 TO dbs211_221zb16;
 
 -- Q21 SOLUTION --
 REVOKE all
-ON newEmployees
+ON new_employees
 FROM dbs211_221zb16;
 
 -- Q22 SOLUTION --
-DROP table newEmployees;
+DROP table new_employees;
 DROP VIEW vwNewEmps;
